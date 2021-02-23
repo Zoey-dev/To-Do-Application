5e9dfec95978a6589214934dd844db445fba1074
Zoey-dev <confidence.peter.7@gmail.com>
2021-02-23 12:06:32 +0100

    Category Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
-import ProgressBar from './Components/ProgressBar/progress'
+// import ProgressBar from './Components/ProgressBar/progress'
+import Signup from './Pages/SignUpPage/signup'
+import Login from './Pages/LoginPage/login'
+import ForgotPassword from './Pages/ForgotPasswordPage/forgotPassword'
+import {Route, BrowserRouter} from 'react-router-dom'
+import {AuthProvider} from './Contexts/AuthContexts'
+
 
 
 
 function App() {
   return (
-     <ProgressBar value = {71}  />
+    //  <ProgressBar value = {71}  />
      
-      /* <Router>
+       <BrowserRouter>
           <AuthProvider>
               <Route exact path="/signup" component={Signup} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/forgotPassword" component={ForgotPassword} />
           </AuthProvider>
-     </Router> */
+     </BrowserRouter>
 
    
   );
